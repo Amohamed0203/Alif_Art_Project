2cd4afcfe46414c5ca80236121a0c2ac88053c24
Ahmed Mohamed <ahmed.moh0203@gmail.com>
2025-01-08 22:40:45 -0600

    Made model

--- AlifArtProject/AlifArtWebsite/models.py
+++ AlifArtProject/AlifArtWebsite/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Gallery(models.Model):
+    image_id = models.IntegerField()
+    image = models.ImageField()
+
+    def return_image_id(self):
+        return self.image_id
+    
+    def return_image(self):
+        return self.image
