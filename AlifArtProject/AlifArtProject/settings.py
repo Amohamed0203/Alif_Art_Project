35d97b202b694c2e677ead2c0955fa77044587b9
Ahmed Mohamed <ahmed.moh0203@gmail.com>
2025-01-10 12:25:59 -0600

    Ready to publish website

--- AlifArtProject/AlifArtProject/settings.py
+++ AlifArtProject/AlifArtProject/settings.py
@@ -74,12 +74,17 @@ WSGI_APPLICATION = 'AlifArtProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
 
 
 # Password validation
